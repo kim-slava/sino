16f6a994e19482cc03423d4d45b951c7304446bc
kim-slava <85705983+kim-slava@users.noreply.github.com>
2021-06-10 22:43:38 +0300

    sait

---
+++ script.js
@@ -0,0 +1,25 @@
+let star=document.getElementById('list')
+
+star.onclick=function(){
+    window.scrollTo({
+        top: 800,
+        behavior:"smooth"
+    })
+}
+
+let star2=document.getElementById('list2')
+star2.onclick=function(){
+    window.scrollTo({
+        top: 1600,
+        behavior:"smooth"
+    })
+}
+
+
+let star3=document.getElementById('down')
+star3.onclick=function(){
+    window.scrollTo({
+        top: 10000,
+        behavior:"smooth"
+    })
+}
